11fa41ad3f1a9c7d1e021c515d50ea469ff694fd
HoussamM <houssammoghraby@gmail.com>
2025-03-31 23:21:14 +0300

    Ordering.Domain: Building domain models and value objects

---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,19 @@
+namespace Ordering.Domain.Models;
+
+public class Order : Aggregate<OrderId>
+{
+    private readonly List<OrderItem> _orderItems = new();
+    private IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+    public CustomerId CustomerId { get; private set; } = default!;
+    public OrderName OrderName { get; private set; } = default!;
+    public Address ShippingAddress { get; private set; } = default!;
+    public Address BillingAddress { get; private set; } = default!;
+    public Payment Payment { get; private set; } = default!;
+    public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+
+    public decimal TotalPrice
+    {
+        get => OrderItems.Sum(o => o.Price * o.Quantity);
+        private set { }
+    }
+}
